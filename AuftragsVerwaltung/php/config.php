ab20ccba1bff10495b0d7afab024418ef249b124
DSigmund <sigmund.dominik@googlemail.com>
2013-09-04 18:16:49 +0200

    Debugging OwnCLoud-Path Css-Z-Index mashing

--- AuftragsVerwaltung/php/config.php
+++ AuftragsVerwaltung/php/config.php
@@ -23,7 +23,7 @@ $oc["server"]="localhost";
 $oc["name"]="owncloud";
 $oc["user"]="owncloud";
 $oc["pass"]="owncloud";
-$oc["basepath"]="/share/MD0_DATA/Auftraege/admin/files/auftraege";
+$oc["basepath"]="/share/MD0_DATA/Auftraege/admin/files/auftraege/";
 $oc["http_link"]="http://192.168.1.18:2507/index.php/apps/files?dir=/Shared/auftraege/";
 
 //vars
